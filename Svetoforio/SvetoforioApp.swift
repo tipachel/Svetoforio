0c2179e32bdcba8c33e2096c291194f055597a0a
tipachel88 <tipakoroche@gmail.com>
2022-01-21 19:09:46 +0300

    Initial Commit

---
+++ Svetoforio/SvetoforioApp.swift
@@ -0,0 +1,17 @@
+//
+//  SvetoforioApp.swift
+//  Svetoforio
+//
+//  Created by Tipachel on 21.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct SvetoforioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
